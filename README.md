c73acaa57924d0d7fd8e8bd42b5467903734ed0a
CWisdomJ <2635814455@qq.com>
2023-09-08 16:45:33 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # BERT4WS
+
+## 依赖项
+
+- transformers
+- pandas
+- torch
+- numpy
+
+## 数据集
+
++ 需要准备一个包含文本数据的CSV文件。在代码示例中，我们提供了两个示例CSV文件：`Mashups.csv`和`APIs.csv`，读者可以根据需要选择其中一个文件，或使用自己的CSV文件。
+
+## 输出文件
+
++ BERT编码后的数据将以`.npy`文件的形式保存在指定的输出文件中。
+
+## 注意事项
+
++ 在代码中，根据数据和需求，需要更改以下部分：
+
+  - 数据文件的路径：修改`data = pd.read_csv("datasets/APIs.csv", encoding='iso-8859-1')`中的文件路径。
+
+  - 输出文件的名称：在`np.save("data_bert_encoding_APIs", bianma_APIs, allow_pickle=True)`中指定要保存BERT编码的输出文件名称。
+
++ 请确保数据文件是适当格式的CSV文件，并且包含一个名为`'Description'`的列，以便代码能够正确读取文本数据。
--- bert2vector.py
+++ bert2vector.py
@@ -1,43 +1,53 @@
+# 导入所需的库
 from transformers import BertModel, BertTokenizer
 import pandas as pd
 import torch
 import numpy as np
 
+# 从Hugging Face模型库中加载预训练的BERT模型和分词器
 model = BertModel.from_pretrained('bert-base-uncased')
-
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-# Mashups
-# data = pd.read_csv("datasets/Mashups.csv", encoding='iso-8859-1')
-# descr = data['description']
+# 从CSV文件中加载数据，这里使用的是APIs.csv文件，可以根据需要注释掉APIs.csv并取消注释Mashups.csv
+# data = pd.read_csv("datasets/Mashups.csv", encoding='iso-8859-1')  # 使用Mashups.csv文件
+data = pd.read_csv("datasets/APIs.csv", encoding='iso-8859-1')  # 使用APIs.csv文件
+descr = data['Description']  # 从数据中提取描述信息列
 
-# APIs
-data = pd.read_csv("datasets/APIs.csv", encoding='iso-8859-1')
-descr = data['Description']
+# 处理NaN值，选择删除包含NaN值的行
+descr = descr.dropna()
 
-
-# 处理NaN值，可以选择填充或删除
-descr = descr.dropna()  # 删除NaN值
-
+# 打印描述数据的形状（行数，列数）
 print(np.shape(descr))
 
+# 将描述文本存储在名为sentences的变量中
 sentences = descr
-bianma = []
-print("program start")
+
+# 创建一个空列表来存储BERT编码后的句子
+bianma_APIs = []
+# bianma_Mashups = []
+
+# 开始处理每个句子
+print("程序开始")
 k = 1
 
 for sentence in sentences:
+    # 使用BERT分词器对句子进行分词和编码，并添加特殊标记，选择最长部分进行截断
     input_ids = tokenizer.encode(sentence, add_special_tokens=True, truncation="longest_first")
+
+    # 将输入编码转换为PyTorch张量
     input_ids = torch.tensor([input_ids])
 
     k += 1
-    print(k, "bianma")
+    print(k, "编码")
+
+    # 使用BERT模型进行推理，获取最后一层的隐藏状态
     with torch.no_grad():
         last_hidden_states = model(input_ids)[0]
-        bianma.append(last_hidden_states[0][0])
+        # 将第一个句子的BERT编码添加到bianma列表中
+        bianma_APIs.append(last_hidden_states[0][0])
+        # bianma_Mashups.append(last_hidden_states[0][0])
         print(np.shape(last_hidden_states[0][0]))
 
-# np.save("data_bert_encoding_Mashups", bianma, allow_pickle=True)  # Mashups
-np.save("data_bert_encoding_APIs", bianma, allow_pickle=True)  # APIs
-
-
+# 保存BERT编码数据到.npy文件中，可以根据需要注释掉Mashups和APIs中的一个
+# np.save("data_bert_encoding_Mashups", bianma_Mashups, allow_pickle=True)  # 保存Mashups的BERT编码
+np.save("data_bert_encoding_APIs", bianma_APIs, allow_pickle=True)  # 保存APIs的BERT编码
